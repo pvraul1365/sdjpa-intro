fe43408d4e1ee6acad968c901a2ba2903b9dc6d3
Raúl Pérez Vicente <rperezv@ayesa.com>
2026-09-07 19:12:07 +0200

    10. Use Spring Initializr to Create Project

---
+++ src/main/java/guru/springframework/sdjpa_intro/SdjpaIntroApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sdjpa_intro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdjpaIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdjpaIntroApplication.class, args);
+	}
+
+}
