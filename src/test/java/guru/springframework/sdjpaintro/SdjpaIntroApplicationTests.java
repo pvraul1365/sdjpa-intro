906cac532c2b32c11d3a66e806d4b8e84bf01edc
Raúl Pérez Vicente <rperezv@ayesa.com>
2026-09-08 18:25:32 +0200

    26. Spring Boot Test

--- src/test/java/guru/springframework/sdjpaintro/SdjpaIntroApplicationTests.java
+++ src/test/java/guru/springframework/sdjpaintro/SdjpaIntroApplicationTests.java
@@ -1,10 +1,22 @@
 package guru.springframework.sdjpaintro;
 
+import guru.springframework.sdjpaintro.repositories.BookRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SdjpaIntroApplicationTests {
+
+	@Autowired
+	BookRepository bookRepository;
+
+	@Test
+	void tstBookRepository() {
+		var count = bookRepository.count();
+
+		assert(count > 0);
+	}
 
 	@Test
 	void contextLoads() {
