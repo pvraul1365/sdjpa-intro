e93f9808d94fb404d171d4c96d49276d491e1d74
Raúl Pérez Vicente <rperezv@ayesa.com>
2026-09-14 21:26:09 +0200

    64. Implement Get Author By ID

--- src/main/java/guru/springframework/sdjpa_intro/dao/AuthorDaoImpl.java
+++ src/main/java/guru/springframework/sdjpa_intro/dao/AuthorDaoImpl.java
@@ -1,6 +1,11 @@
 package guru.springframework.sdjpa_intro.dao;
 
 import guru.springframework.sdjpa_intro.domain.Author;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.sql.DataSource;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 /**
@@ -13,10 +18,38 @@ import org.springframework.stereotype.Component;
  * @since 1.25
  */
 @Component
+@RequiredArgsConstructor
 public class AuthorDaoImpl implements AuthorDao {
 
+    private final DataSource dataSource;
+
     @Override
-    public Author getById(Long id) {
+    public Author getById(final Long id) {
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = dataSource.getConnection();
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM author WHERE id = " + id);
+
+            if (resultSet.next()) {
+                Author author = new Author();
+                author.setId(resultSet.getLong("id"));
+                author.setFirstName(resultSet.getString("first_name"));
+                author.setLastName(resultSet.getString("last_name"));
+
+                return author;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try { if (resultSet != null) resultSet.close(); } catch (Exception e) { e.printStackTrace(); }
+            try { if (statement != null) statement.close(); } catch (Exception e) { e.printStackTrace(); }
+            try { if (connection != null) connection.close(); } catch (Exception e) { e.printStackTrace(); }
+        }
+
         return null;
     }
 
