29fe30e32955cd0a7bfc9fcb611f714d744dfa61
Raúl Pérez Vicente <rperezv@ayesa.com>
2026-09-15 17:56:41 +0200

    68. Refactoring Duplicate Code

--- src/main/java/guru/springframework/sdjpa_intro/dao/AuthorDaoImpl.java
+++ src/main/java/guru/springframework/sdjpa_intro/dao/AuthorDaoImpl.java
@@ -4,6 +4,7 @@ import guru.springframework.sdjpa_intro.domain.Author;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import javax.sql.DataSource;
 import lombok.RequiredArgsConstructor;
@@ -42,19 +43,12 @@ public class AuthorDaoImpl implements AuthorDao {
             // resultSet = statement.executeQuery("SELECT * FROM author WHERE id = " + id);
 
             if (resultSet.next()) {
-                Author author = new Author();
-                author.setId(resultSet.getLong("id"));
-                author.setFirstName(resultSet.getString("first_name"));
-                author.setLastName(resultSet.getString("last_name"));
-
-                return author;
+                return getAuthorFromResultSet(resultSet);
             }
         } catch (Exception e) {
             log.error("Error fetching author by id: {}", id, e);
         } finally {
-            try { if (resultSet != null) resultSet.close(); } catch (Exception e) { log.error("Error closing ResultSet", e); }
-            try { if (preparedStatement != null) preparedStatement.close(); } catch (Exception e) { log.error("Error closing PreparedStatement", e); }
-            try { if (connection != null) connection.close(); } catch (Exception e) { log.error("Error closing Connection", e); }
+            closeAllResources(connection, preparedStatement, resultSet);
         }
 
         return null;
@@ -74,23 +68,29 @@ public class AuthorDaoImpl implements AuthorDao {
             resultSet = preparedStatement.executeQuery();
 
             if (resultSet.next()) {
-                Author author = new Author();
-                author.setId(resultSet.getLong("id"));
-                author.setFirstName(resultSet.getString("first_name"));
-                author.setLastName(resultSet.getString("last_name"));
-
-                return author;
+                return getAuthorFromResultSet(resultSet);
             }
 
         } catch (Exception e) {
             log.error("Error fetching author by name: {} {}", firstName, lastName, e);
         } finally {
-            try { if (resultSet != null) resultSet.close(); } catch (Exception e) { log.error("Error closing ResultSet", e); }
-            try { if (preparedStatement != null) preparedStatement.close(); } catch (Exception e) { log.error("Error closing PreparedStatement", e); }
-            try { if (connection != null) connection.close(); } catch (Exception e) { log.error("Error closing Connection", e); }
+            closeAllResources(connection, preparedStatement, resultSet);
         }
 
         return null;
     }
 
+    private void closeAllResources(Connection connection, PreparedStatement preparedStatement, ResultSet resultSet) {
+        try { if (resultSet != null) resultSet.close(); } catch (Exception e) { log.error("Error closing ResultSet", e); }
+        try { if (preparedStatement != null) preparedStatement.close(); } catch (Exception e) { log.error("Error closing PreparedStatement", e); }
+        try { if (connection != null) connection.close(); } catch (Exception e) { log.error("Error closing Connection", e); }
+    }
+
+    private Author getAuthorFromResultSet(ResultSet resultSet) throws SQLException {
+        Author author = new Author();
+        author.setId(resultSet.getLong("id"));
+        author.setFirstName(resultSet.getString("first_name"));
+        author.setLastName(resultSet.getString("last_name"));
+        return author;
+    }
 }
