f390d797673f6fb8a49abd09527659339f78de36
Raúl Pérez Vicente <rperezv@ayesa.com>
2026-09-19 09:52:53 +0200

    Asignment 10: Implement Book DAO

---
+++ src/main/java/guru/springframework/jdbc/dao/BookDaoImpl.java
@@ -0,0 +1,75 @@
+package guru.springframework.jdbc.dao;
+
+import guru.springframework.jdbc.domain.Book;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.TypedQuery;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+/**
+ * BookDaoImpl
+ * <p>
+ * Created by IntelliJ, Spring Framework Guru.
+ *
+ * @author architecture - raul.perez.vicente@gmail.com
+ * @version 19/09/2026 - 09:13
+ * @since 1.25
+ */
+@Component
+@RequiredArgsConstructor
+public class BookDaoImpl implements BookDao {
+
+    private final EntityManagerFactory entityManagerFactory;
+
+    @Override
+    public Book getById(final Long id) {
+        return this.getEntityManager().find(Book.class, id);
+    }
+
+    @Override
+    public Book findBookByTitle(final String title) {
+        EntityManager entityManager = this.getEntityManager();
+
+        TypedQuery<Book> query = entityManager.createQuery(
+                "SELECT b FROM Book b WHERE b.title = :title", Book.class);
+        query.setParameter("title", title);
+
+        return query.getSingleResult();
+    }
+
+    @Override
+    public Book saveNewBook(final Book book) {
+        EntityManager entityManager = this.getEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(book);
+        entityManager.getTransaction().commit();
+
+        return book;
+    }
+
+    @Override
+    public Book updateBook(final Book book) {
+        EntityManager entityManager = this.getEntityManager();
+        entityManager.getTransaction().begin();
+        Book updatedBook = entityManager.merge(book);
+        entityManager.getTransaction().commit();
+
+        return updatedBook;
+    }
+
+    @Override
+    public void deleteBookById(final Long id) {
+        EntityManager entityManager = this.getEntityManager();
+        entityManager.getTransaction().begin();
+        Book book = entityManager.find(Book.class, id);
+        if (book != null) {
+            entityManager.remove(book);
+        }
+        entityManager.getTransaction().commit();
+    }
+
+    private EntityManager getEntityManager() {
+        return this.entityManagerFactory.createEntityManager();
+    }
+}
---
+++ src/test/java/guru/springframework/jdbc/BookDaoIntegrationTest.java
@@ -0,0 +1,103 @@
+package guru.springframework.jdbc;
+
+import guru.springframework.jdbc.dao.BookDao;
+import guru.springframework.jdbc.dao.BookDaoImpl;
+import guru.springframework.jdbc.domain.Book;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+/**
+ * BookDaoIntegrationTest
+ * <p>
+ * Created by IntelliJ, Spring Framework Guru.
+ *
+ * @author architecture - raul.perez.vicente@gmail.com
+ * @version 19/09/2026 - 09:36
+ * @since 1.25
+ */
+@ActiveProfiles("local")
+@DataJpaTest
+@Import({ BookDaoImpl.class })
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Slf4j
+public class BookDaoIntegrationTest {
+
+    @Autowired
+    BookDao bookDao;
+
+    @Test
+    void testGetBook() {
+        Long bookId = 1L; // Replace with a valid book ID from your database
+        var book = bookDao.getById(bookId);
+        assert book != null;
+
+        log.info("Book: {}", book);
+    }
+
+    @Test
+    void testGetBookByTitle() {
+        String title = "Spring in Action, 5th Edition"; // Replace with a valid title from your database
+        var book = bookDao.findBookByTitle(title);
+        assert book != null;
+    }
+
+    @Test
+    void testSaveBook() {
+        var newBook = new Book();
+        newBook.setTitle("New Book Title");
+        newBook.setIsbn("1234567890");
+        newBook.setPublisher("Test Publisher");
+        newBook.setAuthorId(1L); // Replace with a valid author ID from your database
+
+        var savedBook = bookDao.saveNewBook(newBook);
+        assert savedBook != null;
+        assert savedBook.getId() != null;
+
+        log.info("Saved Book: {}", savedBook);
+    }
+
+    @Test
+    void testUpdateBook() {
+        var newBook = new Book();
+        newBook.setTitle("New Book Title");
+        newBook.setIsbn("1234567890");
+        newBook.setPublisher("Test Publisher");
+        newBook.setAuthorId(1L);
+        var savedBook = bookDao.saveNewBook(newBook);
+        assert savedBook != null;
+        assert savedBook.getId() != null;
+
+        savedBook.setTitle("Updated Book Title");
+
+        var updatedBook = bookDao.updateBook(savedBook);
+        assert updatedBook != null;
+        assert updatedBook.getTitle().equals("Updated Book Title");
+
+        log.info("Updated Book: {}", updatedBook);
+    }
+
+    @Test
+    void testDeleteBook() {
+        var newBook = new Book();
+        newBook.setTitle("New Book Title");
+        newBook.setIsbn("1234567890");
+        newBook.setPublisher("Test Publisher");
+        newBook.setAuthorId(1L);
+        var savedBook = bookDao.saveNewBook(newBook);
+        assert savedBook != null;
+        assert savedBook.getId() != null;
+
+        bookDao.deleteBookById(savedBook.getId());
+
+        var deletedBook = bookDao.getById(savedBook.getId());
+        assert deletedBook == null;
+
+        log.info("Deleted Book with ID: {}", savedBook.getId());
+    }
+
+}
