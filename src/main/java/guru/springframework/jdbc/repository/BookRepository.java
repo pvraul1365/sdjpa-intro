a0bb4147ce9b194ebe4fe17f24ec4b397c47db8d
Raúl Pérez Vicente <rperezv@ayesa.com>
2026-09-20 12:57:26 +0200

    107. Named Parameters with @Query

--- src/main/java/guru/springframework/jdbc/repository/BookRepository.java
+++ src/main/java/guru/springframework/jdbc/repository/BookRepository.java
@@ -7,6 +7,7 @@ import java.util.concurrent.Future;
 import java.util.stream.Stream;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.scheduling.annotation.Async;
 
 /**
@@ -34,4 +35,7 @@ public interface BookRepository extends JpaRepository<Book, Long> {
 
     @Query("SELECT b FROM Book b WHERE b.title = ?1")
     Book findBookByTitleWithQuery(String title);
+
+    @Query("SELECT b FROM Book b WHERE b.title = :title")
+    Book findBookByTitleWithQueryNamed(@Param("title") String title);
 }
